473eafa1cac60b2c030d9a4d4bba3b114ad85b91
chverma <538853+chverma@users.noreply.github.com>
2021-04-21 15:36:35 +0200

    Add docker compose to install it

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
  sudo apt -qq update
 
- sudo apt -qq install -y docker-ce docker-ce-cli containerd.io
+ sudo apt -qq install -y docker-ce docker-ce-cli containerd.io docker-compose
 fi
 
 ##############################################################
